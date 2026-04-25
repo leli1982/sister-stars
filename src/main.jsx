f39fcb26881fe4fd09681030d2481703b7f3faf7
Leli Apap <lelimacbook@Lelis-MacBook-Pro.local>
2026-04-26 00:51:46 +0200

    stable magic stars working version

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-import { registerSW } from "virtual:pwa-register";
-
 // ⚡ Enable installable PWA service worker
-registerSW({ immediate: true });
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      includeAssets: ["favicon.ico"],
-      manifest: {
-        name: "Magic Sister Stars",
-        short_name: "Stars",
-        start_url: "/",
-        display: "standalone",
-        background_color: "#ffffff",
-        theme_color: "#ffb6ff",
-        icons: [
-          {
-            src: "icon-192.png",
-            sizes: "192x192",
-            type: "image/png"
-          },
-          {
-            src: "icon-512.png",
-            sizes: "512x512",
-            type: "image/png"
-          }
-        ]
-      }
-    })
-  ]
+  plugins: [react()]
 });
